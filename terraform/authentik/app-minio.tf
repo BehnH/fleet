a25beb434d6f32519edcd467caed998e8b6b1767
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-22 17:32:41 +0100

    tf: More MinIO updates in authentik

--- terraform/authentik/app-minio.tf
+++ terraform/authentik/app-minio.tf
@@ -8,6 +8,15 @@ resource "authentik_provider_oauth2" "oauth-provider_minio" {
   redirect_uris = [
     "https://s3-console.behn.dev/oauth_callback"
   ]
+
+  property_mappings = [
+    "29821f19-e935-42ca-8339-10b234af36c2",
+    "a430abec-cb70-462f-84d7-4bcd569fda5e",
+    "92f365c4-36cd-4af0-81dc-fdaec134c49c",
+    "fb30e55b-bfed-43a4-b2b3-d9108edb939b"
+  ]
+
+  token_validity = "minutes=30"
 }
 
 resource "authentik_application" "oauth-app_minio" {
@@ -16,4 +25,6 @@ resource "authentik_application" "oauth-app_minio" {
   group = "Storage"
 
   meta_description = "MinIO is a High-Performance Object Storage application, designed with S3 API compatibility."
+
+  protocol_provider = authentik_provider_oauth2.oauth-provider_minio.id
 }
