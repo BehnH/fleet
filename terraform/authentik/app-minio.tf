67235b8428a88a47439ed9322ba24764595ec717
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-22 17:08:04 +0100

    tf: Add client_id I guess?

--- terraform/authentik/app-minio.tf
+++ terraform/authentik/app-minio.tf
@@ -2,6 +2,8 @@ resource "authentik_provider_oauth2" "oauth-provider_minio" {
   name               = "MinIO"
   authorization_flow = data.authentik_flow.default-authorization-flow
 
+  client_id = "QeK4sLaowIn11fdmfPa6atsk2YW19ObOx4itVb0f"
+
   signing_key = "c30c6593-c9b3-46b3-a538-b8a8c3885367"
   redirect_uris = [
     "https://s3-console.behn.dev/oauth_callback"
