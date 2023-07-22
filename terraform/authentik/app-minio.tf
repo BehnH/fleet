166950a8d8b8fc96bc03653654b0c47aae16c336
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-22 17:05:57 +0100

    tf: Forgot to add minio authentik

---
+++ terraform/authentik/app-minio.tf
@@ -0,0 +1,17 @@
+resource "authentik_provider_oauth2" "oauth-provider_minio" {
+  name               = "MinIO"
+  authorization_flow = data.authentik_flow.default-authorization-flow
+
+  signing_key = "c30c6593-c9b3-46b3-a538-b8a8c3885367"
+  redirect_uris = [
+    "https://s3-console.behn.dev/oauth_callback"
+  ]
+}
+
+resource "authentik_application" "oauth-app_minio" {
+  name  = "MinIO"
+  slug  = "minio"
+  group = "Storage"
+
+  meta_description = "MinIO is a High-Performance Object Storage application, designed with S3 API compatibility."
+}
