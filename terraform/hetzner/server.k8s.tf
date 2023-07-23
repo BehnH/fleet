7a9d724bf65093f78d8489e096b3f6ecdb07029d
Behn <7383025+BehnH@users.noreply.github.com>
2023-07-23 11:42:45 +0100

    tf: Remove master-2

--- terraform/hetzner/server.k8s.tf
+++ terraform/hetzner/server.k8s.tf
@@ -9,30 +9,6 @@ resource "hcloud_server" "k8s_master-1" {
   }
 }
 
-# resource "hcloud_server" "k8s_master-2" {
-#   name        = "master-2"
-#   location    = "fsn1"
-#   server_type = "cax11"
-#   image       = "119912350"
-#
-#   firewall_ids = [
-#     local.k8s_master_fw_id,
-#     local.k8s_worker_fw_id
-#   ]
-#
-#   network {
-#     network_id = local.k8s_network_id
-#   }
-#
-#   ssh_keys = [
-#     data.hcloud_ssh_key.personal_ssh_key.id
-#   ]
-#
-#   labels = {
-#     "k8s-type" = "master"
-#   }
-# }
-
 resource "hcloud_server" "k8s_worker-1" {
   name        = "worker-1"
   location    = "fsn1"
